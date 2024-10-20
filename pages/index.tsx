426ded9bc7eb6472922a04bdef30b55ad9f5aa01
Ghalya AL-Eshaq <ghalyaal-eshaq@dyn-172-20-101-122.qatar.cmu.local>
2024-10-20 17:16:07 +0300

    added my andrew id

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         }
       />
       <div>
-        {QueryProcessor(query) == "" ? "No Match" : QueryProcessor(query)}
+        {QueryProcessor(query) == "" ? "No Match hehehe" : QueryProcessor(query)}
       </div>
     </div>
   );
